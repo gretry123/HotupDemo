c23dc995d28f2f5bf2f5789c03d00a936375317b
gretry <gretry@foxmail.com>
2022-07-07 16:53:16 +0800

    init

---
+++ assets/bundle_home/scripts/tabs/TabCtrl.ts
@@ -0,0 +1,44 @@
+import { _decorator, Component, Sprite, Node, Animation, SpriteFrame, Vec3 } from "cc";
+import { MenuSidebar } from "./MenuSidebar";
+const { ccclass, property } = _decorator;
+
+export interface ITabCtrlInfo{
+    sidebar: MenuSidebar;
+    idx: number;
+    iconSF: SpriteFrame;
+}
+
+@ccclass
+export class TabCtrl extends Component {
+    @property
+    public idx = 0;
+    @property(Sprite)
+    public icon: Sprite | null = null;
+    @property(Node)
+    public arrow: Node | null = null;
+    @property(Animation)
+    public anim: Animation | null = null;
+
+    public sidebar: MenuSidebar | null = null;
+
+    // use this for initialization
+    init(tabInfo: ITabCtrlInfo) { // sidebar, idx, iconSF
+        this.sidebar = tabInfo.sidebar;
+        this.idx = tabInfo.idx;
+        this.icon.spriteFrame = tabInfo.iconSF;
+        this.node.on('touchstart', this.onPressed.bind(this), this.node);
+        this.arrow.scale = new Vec3(0, 0);
+    }
+
+    onPressed() {
+        this.sidebar.tabPressed(this.idx);
+    }
+
+    turnBig() {
+        this.anim.play('tab_turn_big');
+    }
+
+    turnSmall() {
+        this.anim.play('tab_turn_small');
+    }
+}
